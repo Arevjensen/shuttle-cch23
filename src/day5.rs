e9a4c8dff24ad0605071636e543e8cba6caf0926
Are Velde Jensen <are.velde.jensen@nrk.no>
2023-12-27 11:38:47 +0100

    day 5 part 2

--- src/day5.rs
+++ src/day5.rs
@@ -4,7 +4,7 @@ use axum::{
     routing::post,
     Json, Router,
 };
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 
 pub fn day5() -> Router {
     Router::new().route("/5", post(slicing_the_loop))
@@ -14,12 +14,35 @@ async fn slicing_the_loop(
     stuff: Query<Stuff>,
     extract::Json(payload): extract::Json<Vec<String>>,
 ) -> impl IntoResponse {
-    let result = payload.as_slice()[stuff.offset..stuff.offset + stuff.limit].to_vec();
+    println!("this is a new request");
+    println!("query paramts: {:?}, vec: {:?}", stuff, payload);
+    let offset = stuff.offset.unwrap_or(0).clamp(0, payload.len());
+    let limit = stuff.limit.unwrap_or(payload.len()).clamp(0, payload.len());
+    let limit = (limit + offset).clamp(0, payload.len());
 
-    Json(result)
+    println!("offset: {}, limit: {}", offset, limit);
+
+    let result = payload.as_slice()[offset..limit].to_vec();
+    if let Some(splitting) = stuff.split {
+        let some_result = result
+            .chunks(splitting)
+            .map(|x| x.to_vec())
+            .collect::<Vec<Vec<String>>>();
+        return Json(Things::VecOfVec(some_result));
+    }
+
+    Json(Things::SingleVec(result))
 }
-#[derive(Deserialize)]
+
+#[derive(Deserialize, Debug)]
 struct Stuff {
-    offset: usize,
-    limit: usize,
+    offset: Option<usize>,
+    limit: Option<usize>,
+    split: Option<usize>,
+}
+#[derive(Deserialize, Serialize, Debug)]
+#[serde(untagged)]
+enum Things {
+    SingleVec(Vec<String>),
+    VecOfVec(Vec<Vec<String>>),
 }
